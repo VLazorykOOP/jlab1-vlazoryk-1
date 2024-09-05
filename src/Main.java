3fa3c5e25d1c7c4a0ddc027e3d315867489f9428
Vasyl <vlazoryk@chnu.edu.ua>
2024-09-05 10:00:54 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Lab 1 Java");
+        //
         System.out.printf("test");
     }
 }
